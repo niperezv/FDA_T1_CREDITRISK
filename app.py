ae498ad6a8ac05cc0e54943dc9f83b7274642c5b
pePOOni <47955066+niperezv@users.noreply.github.com>
2023-09-27 02:22:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,5 +40,7 @@ def predict():
         # Devolver la respuesta, por ejemplo, la predicción
         return render_template('index.html', prediction_text='Resultado: {}'.format(prediction))
 
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=port)
